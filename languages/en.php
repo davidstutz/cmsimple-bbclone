16d3daf4bf5668bfe8cad3f4315a83a8c129f47f
manu <marinello@pixolution.ch>
2018-01-13 17:19:55 +0100

    bbclone.php removed/moved to classes/BBClone.php language files tooltip added config/metaconfig.php added for config checkbox

--- languages/en.php
+++ languages/en.php
@@ -10,4 +10,5 @@
 	$plugin_tx['bbclone']['See your statistics']="See your statistics";
 	$plugin_tx['bbclone']['Go to statistics.']="Go to statistics";
 	$plugin_tx['bbclone']['The statistics will be displayed using an external tool to get the best possible overview over the user\'s behaviour on your website.']="The statistics will be displayed using an external tool to get the best possible overview over the user\'s behaviour on your website."
+	$plugin_tx['bbclone']['cf_track_backend']="check if the web counter should track the backend as well, uncheck if not.";
 ?>
